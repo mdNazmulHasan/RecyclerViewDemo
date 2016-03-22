322d8489de4821894c32c9a13d785fd7aa8913bb
Nazmul Hasan <najmul.hasan10@gmail.com>
2016-03-22 16:49:18 +0600

    Initial commit

---
+++ app/src/main/java/bhouse/travellist/MainActivity.java
@@ -0,0 +1,111 @@
+package bhouse.travellist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    private RecyclerView mRecyclerView;
+    private StaggeredGridLayoutManager mStaggeredLayoutManager;
+    private TravelListAdapter mAdapter;
+    private boolean isListView;
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setUpActionBar();
+
+        mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
+
+        mRecyclerView.setHasFixedSize(true); //Data size is fixed - improves performance
+        mAdapter = new TravelListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        //mAdapter.setOnItemClickListener(onItemClickListener);
+
+        isListView = true;
+    }
+
+    /*TravelListAdapter.OnItemClickListener onItemClickListener = new TravelListAdapter.OnItemClickListener() {
+        @Override
+        public void onItemClick(View v, int position) {
+            Intent transitionIntent = new Intent(MainActivity.this, DetailActivity.class);
+            transitionIntent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+            ImageView placeImage = (ImageView) v.findViewById(R.id.placeImage);
+            LinearLayout placeNameHolder = (LinearLayout) v.findViewById(R.id.placeNameHolder);
+
+            View navigationBar = findViewById(android.R.id.navigationBarBackground);
+            View statusBar = findViewById(android.R.id.statusBarBackground);
+
+            Pair<View, String> imagePair = Pair.create((View) placeImage, "tImage");
+            Pair<View, String> holderPair = Pair.create((View) placeNameHolder, "tNameHolder");
+            Pair<View, String> navPair = Pair.create(navigationBar, Window.NAVIGATION_BAR_BACKGROUND_TRANSITION_NAME);
+            Pair<View, String> statusPair = Pair.create(statusBar, Window.STATUS_BAR_BACKGROUND_TRANSITION_NAME);
+            Pair<View, String> toolbarPair = Pair.create((View) toolbar, "tActionBar");
+
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, imagePair, holderPair, navPair, statusPair, toolbarPair);
+            ActivityCompat.startActivity(MainActivity.this, transitionIntent, options.toBundle());
+        }
+    };*/
+
+    private void setUpActionBar() {
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            /*getActionBar().setDisplayHomeAsUpEnabled(false);
+            getActionBar().setDisplayShowTitleEnabled(true);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getActionBar().setElevation(7);
+            }*/
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_toggle) {
+            toggle();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggle() {
+        MenuItem item = menu.findItem(R.id.action_toggle);
+        if (isListView) {
+            mStaggeredLayoutManager.setSpanCount(2);
+            item.setIcon(R.drawable.ic_action_list);
+            item.setTitle("Show as list");
+            isListView = false;
+        } else {
+            mStaggeredLayoutManager.setSpanCount(1);
+            item.setIcon(R.drawable.ic_action_grid);
+            item.setTitle("Show as grid");
+            isListView = true;
+        }
+    }
+}
